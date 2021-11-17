87c413520c95f5fc5a8d1206eac8255281d25b3b
Nanda Aditya Putra <88027838+nandaditra@users.noreply.github.com>
2021-11-17 22:55:09 +0700

    Update init.php

--- init.php
+++ init.php
@@ -12,4 +12,6 @@ require_once 'model/modelPengirim.php';
 require_once 'model/modelKurir.php';
 require_once 'model/modelStatus.php';
 require_once 'model/modelBarang.php';
+require_once 'model/modelRiwayatPengiriman';
+require_once 'model/modelPengiriman';
 ?>
